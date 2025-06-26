7402e35ba7f7f56f00a4eaafc3d0c750d82b67ec
mitan11 <mitantank00@gmail.com>
2025-06-26 13:34:09 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,28 @@
+print('Hello , World!')
+
+# We have to use '#' for writing a comment in python.
+
+"""
+
+Multiline comments are not available in python but we can
+achieve it by using Doc String
+
+"""
+
+# Variables in python
+# Variables are used to store data in python. We can assign a value to a variable using '='
+Name = "Mitan"
+Age = 12
+
+# Data Types in python
+# Python has several built-in data types. Some of them are:
+# 1. Integers: These are whole numbers, either positive, negative or zero.
+# 2. Float: These are decimal numbers.
+# 3. String: These are sequences of characters.
+# 4. Boolean: These are true or false values.
+# 5. List: These are ordered collections of items that can be of any data type.
+# 6. Tuple: These are ordered, immutable collections of items that can be of any data
+# 7. Dictionary: These are unordered collections of key-value pairs.
+# 8. Set: These are unordered collections of unique items.
+# 9. Complex: These are complex numbers, consisting of a real and imaginary part.
+
---
+++ q1.py
@@ -0,0 +1,5 @@
+num = int(input("Enter Any Number : "))
+Age = int(input("Enter your Age : "))
+
+print(num)
+print(f"{Age}")
