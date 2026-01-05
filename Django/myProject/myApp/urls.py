fbdae28281fc3b34026222f9515f233da3e6e69b
Mitan tank <141761215+Mitan11@users.noreply.github.com>
2026-01-05 16:47:38 +0530

    Add homepage, about, and contact pages with navigation and styling

--- Django/myProject/myApp/urls.py
+++ Django/myProject/myApp/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.urls import path
 from . import views
 
+app_name = 'myApp'
+
 urlpatterns = [
-    path('home/' , views.home , name='home')
+    path('home/' , views.home , name='home'),
+    path('about/' , views.about , name='about'),
+    path('contact/' , views.contact , name='contact'),
 ]
